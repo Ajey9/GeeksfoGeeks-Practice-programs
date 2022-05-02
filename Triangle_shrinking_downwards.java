5530d36af49c4b5861276dac65648cedbb91b9ee
gathadi <ajaygathadi@gmail.com>
2022-05-02 19:12:42 +0530

    -

---
+++ Triangle_shrinking_downwards.java
@@ -0,0 +1,35 @@
+package GeeksforGeeks;
+
+import java.io.*;
+
+class Triangle_shrinking_downwards {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the string");
+        String string = bufferedReader.readLine();
+
+        String str = Word_Pattern_Solve.pattern(string);
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            System.out.print(str.charAt(i));
+            if((i+1) == str.length())
+                System.out.println();
+        }
+    }
+}
+
+class Word_Pattern_Solve
+{
+    static String pattern(String s)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i=0;i<s.length();i++)
+        {
+            stringBuilder.append(".".repeat(i)); //@method '.repeat(i)' appends the "." i times.
+            stringBuilder.append(s.substring(i));
+        }
+        return stringBuilder.toString();
+    }
+}
