0c0dc435ffde480b14aba4505d21213f71d9ca0f
gathadi <ajaygathadi@gmail.com>
2022-05-16 18:11:27 +0530

    16th May 2022

---
+++ Basic/Diagonal_SUm.java
@@ -0,0 +1,45 @@
+package GeeksforGeeks.Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Diagonal_SUm {public static void main(String[] args) throws IOException
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(br.readLine().trim());
+            while(T-->0)
+            {
+                int n = Integer.parseInt(br.readLine().trim());
+                int[][] matrix = new int[n][n];
+                for(int i = 0; i < n; i++){
+                    String[] S = br.readLine().trim().split(" ");
+                    for(int j = 0; j < n; j++)
+                        matrix[i][j] = Integer.parseInt(S[j]);
+                }
+                Mat ob = new Mat();
+                int ans = ob.DiagonalSum(matrix);
+                System.out.println(ans);
+            }
+        }
+}
+
+class Mat
+    {
+        public int DiagonalSum(int[][] matrix)
+        {
+            int sum = 0;
+            for(int i=0;i<matrix.length;i++)
+            {
+                for(int j=0;j<matrix.length;j++)
+                {
+                    if(i==j)
+                    {
+                        sum = sum + matrix[i][j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+
---
+++ Basic/Party_of_Couples.java
@@ -0,0 +1,37 @@
+package GeeksforGeeks.Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * This program find out which element is occuring only once in a given array.
+ */
+public class Party_of_Couples {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            String[] input_line = read.readLine().trim().split("\\s+");
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+
+            System.out.println(Single.findSingle(N, arr));
+        }
+    }
+}
+class Single {
+    static int findSingle(int N, int[] arr) {
+        int single = 0;
+
+        for(int i=0;i<N;i++)
+        {
+           single = single ^ arr[i];
+            System.out.println(single);
+        }
+        return single;
+    }
+}
+
